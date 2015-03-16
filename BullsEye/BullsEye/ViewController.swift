c34775165e49d2abdd5625e19abfdab7128f4f02
KQiangMac <KevinQiang.K@gmail.com>
2015-03-16 23:00:29 +0800

    finished using method update ui label's text.

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -11,26 +11,41 @@ import UIKit
 class ViewController: UIViewController {
 
     var currentValue:Int = 0
+    var targetValue:Int = 0
     
     @IBOutlet weak var slider:UISlider!
-    
+    @IBOutlet weak var targetLabel:UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        currentValue = lroundf(slider.value)
-        // Do any additional setup after loading the view, typically from a nib.
+        startNewRound()
+        updateLabels()
     }
 
+    func startNewRound(){
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = lroundf(slider.value)
+        slider.value = Float(currentValue)
+    }
+    
+    func updateLabels(){
+        targetLabel.text = String(targetValue)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
     @IBAction func showAlertMessage(){
-        let message = "The valude of the slider is now:\(currentValue)"
+        let message = "The current value is: \(currentValue)"
+                    + "\nThe target value is: \(targetValue)";
+        
         let alert = UIAlertController(title: "You Guess!", message: message, preferredStyle: .Alert)
         let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
         alert.addAction(action)
         presentViewController(alert, animated: true, completion: nil)
+        startNewRound()
+        updateLabels()
     }
 
     @IBAction func slideMove(slider:UISlider){
