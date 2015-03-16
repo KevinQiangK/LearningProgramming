265864b79554f926e21cc967cd32a9f0345f5cd1
KQiangMac <KevinQiang.K@gmail.com>
2015-03-16 22:28:01 +0800

    learning the @UIOutlet for store UISlider variable.

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -10,10 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var currentValue:Int = 50
+    var currentValue:Int = 0
+    
+    @IBOutlet weak var slider:UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        currentValue = lroundf(slider.value)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
