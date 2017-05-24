0104e8b8263f1c4dd8cf59cf133655b2a4dff5a1
KQiangMac <KevinQiang.K@gmail.com>
2017-05-25 00:39:55 +0800

    feat: crate the home page view.

--- python/lightweight_django/placeholder/placeholder.py
+++ python/lightweight_django/placeholder/placeholder.py
@@ -12,7 +12,7 @@ from django.conf import settings
 DEBUG = os.environ.get('DEBUG', 'on') == 'on'
 SECRET_KEY = os.environ.get('SECRET_KEY', 'pv0^7c$a^u(wp+-3ksswb0k0=@tuv_64&7y@!rni#ub-q642d=')
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
-
+BASE_DIR = os.path.dirname(__file__)
 
 # configure the settings before makeing any additional imports from Django.
 settings.configure(
@@ -24,16 +24,30 @@ settings.configure(
         'django.middleware.common.CommonMiddleware',
         'django.middleware.csrf.CsrfViewMiddleware',
         'django.middleware.clickjacking.XFrameOptionsMiddleware'
-    )
+    ),
+    INSTALLED_APPS=(
+        'django.contrib.staticfiles',
+    ),
+    TEMPLATES=(
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'DIRS': (os.path.join(BASE_DIR, 'templates'), ),
+        },
+    ),
+    STATICFILES_DIRS=(
+        os.path.join(BASE_DIR, 'static'),
+    ),
+    STATIC_URL='/static/',
 )
 
-
+from django import forms
 from django.conf.urls import url
 from django.core.wsgi import get_wsgi_application
 from django.core.cache import cache
+from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseBadRequest
 from django.views.decorators.http import etag
-from django import forms
+from django.shortcuts import render
 
 from io import BytesIO
 from PIL import Image, ImageDraw
@@ -83,17 +97,22 @@ def placeholder(request, width, height):
         return HttpResponseBadRequest('Invalid Image Request.')
 
 
-def index():
-    return HttpResponse('Home')
+def index(request):
+    example = reverse('placeholder', kwargs={'width': 50, 'height': 50})
+    context = {
+        'example': request.build_absolute_uri(example)
+    }
+    return render(request, 'home.html', context)
+
 
 urlpatterns = (
     url(r'^image/(?P<width>[0-9]+)x(?P<height>[0-9]+)/$', placeholder, name='placeholder'),
     url(r'^$', index, name='homepage'),
 )
 
-
 application = get_wsgi_application()
 
 if __name__ == "__main__":
     from django.core.management import execute_from_command_line
+
     execute_from_command_line(sys.argv)
